8cf34aa3ebd5a8355569ded60b4059cf33f5a6c3
James Tanner <tanner.jc@gmail.com>
2024-02-01 12:17:11 -0500

    Add mixins.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- ansible_base/authentication/authenticator_plugins/github.py
+++ ansible_base/authentication/authenticator_plugins/github.py
@@ -5,8 +5,13 @@ from django.urls import reverse
 from django.contrib.auth.backends import ModelBackend
 from django.core.exceptions import ValidationError
 
+from social_core.backends.github import GithubOAuth2
+
 from ansible_base.authentication.authenticator_plugins.base import AbstractAuthenticatorPlugin, BaseAuthenticatorConfiguration
 from ansible_base.authentication.models import AuthenticatorUser
+
+from ansible_base.authentication.social_auth import SocialAuthMixin
+from ansible_base.lib.serializers.fields import CharField, URLField
 
 logger = logging.getLogger('ansible_base.authentication.authenticator_plugins.local')
 
@@ -39,7 +44,7 @@ class GithubConfiguration(BaseAuthenticatorConfiguration):
     )
 
     SOCIAL_AUTH_GITHUB_KEY = CharField(
-        help_text=_('The OAuth2 key (Client ID) from your GitHub developer application.'),
+        help_text=('The OAuth2 key (Client ID) from your GitHub developer application.'),
         allow_null=False,
         ui_field_label=('GitHub OAuth2 Key'),
     )
@@ -51,7 +56,7 @@ class GithubConfiguration(BaseAuthenticatorConfiguration):
     )
 
 
-class AuthenticatorPlugin(ModelBackend, AbstractAuthenticatorPlugin):
+class AuthenticatorPlugin(SocialAuthMixin, GithubOAuth2, AbstractAuthenticatorPlugin):
     configuration_class = GithubConfiguration
     logger = logger
     type = "github"
